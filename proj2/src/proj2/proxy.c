616866ab4f09ef50d8e09456eda85e33f9eadb1e
Lee Byeoksan <lee.byeoksan+github@gmail.com>
2015-09-25 23:01:13 +0900

    ready for project2

---
+++ proj2/src/proj2/proxy.c
@@ -0,0 +1,20 @@
+/*
+ * proxy.c
+ * 
+ * EE324 Assignment 2
+ * Part I   - sequential web proxy (not implemented)
+ * Part II  - concurrent web proxy (not implemented)
+ * Part III - caching web objects (not implemented)
+ *
+ * Author: Lee, Byeoksan <lbs6170@kaist.ac.kr>
+ */
+
+int main(int argc, char *argv[])
+{
+    /* open a socket */
+    /* listen */
+    /* accept */
+    /* parse the request */
+    /* wait for response from end server */
+    /* forward the response to user */
+}
